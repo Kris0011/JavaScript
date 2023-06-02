fbda7733af820e26c911c5e68f8492853c54cf1d
Kris Patel <krishp759@gmail.com>
2023-06-02 10:48:26 +0530

    Style changed for MathZone and added sqrt

--- Maths/script.js
+++ Maths/script.js
@@ -1,15 +1,15 @@
 
-document.getElementById("myForm").addEventListener("submit", function(event) {
+document.getElementById("myForm").addEventListener("submit", function (event) {
   event.preventDefault();
   var m = document.getElementById(`method`).value;
   console.log(m)
-  if(m == "Euler Method"){
+  if (m == "Euler Method") {
     solveEuler();
   }
-  else if( m == "Improved Euler Method"){
+  else if (m == "Improved Euler Method") {
     solveImprovedEuler();
   }
-  else if( m == "R-K Method"){
+  else if (m == "R-K Method") {
     solveRK();
   }
 });
@@ -24,8 +24,16 @@ function solveEuler() {
 
   // Define the derivative function (dy/dx)
   function derivative(x, y) {
-    const func = new Function("x", "y", `return (${myFunction})`);
-    return func(x, y);
+    if (myFunction.includes("sqrt")) {
+      const func = new Function("x", "y", `return Math.sqrt(y)`);
+      return func(x, y);
+    } else if (myFunction.includes("log")) {
+      const func = new Function("x", "y", `return Math.log(y)`);
+      return func(x, y);
+    } else {
+      const func = new Function("x", "y", `return (${myFunction})`);
+      return func(x, y);
+    }
   }
 
   // Euler's method for solving the initial value problem
@@ -40,7 +48,7 @@ function solveEuler() {
       results += `x = ${x.toFixed(4)}, y = ${y.toFixed(4)}<br>`;
       y += h * slope;
       x += h;
-      x = parseFloat(x.toFixed(100)); // Handle floating-point precision error
+      x = parseFloat(x.toFixed(10)); // Handle floating-point precision error
     }
 
     return results;
@@ -53,6 +61,11 @@ function solveEuler() {
 }
 
 
+
+
+
+
+
 function solveImprovedEuler() {
   const myFunction = document.getElementById("functionInput").value;
   const x0 = parseFloat(document.getElementById("x0Input").value);
@@ -62,8 +75,16 @@ function solveImprovedEuler() {
 
   // Define the derivative function (dy/dx)
   function derivative(x, y) {
-    const func = new Function("x", "y", `return (${myFunction})`);
-    return func(x, y);
+    if (myFunction.includes("sqrt")) {
+      const func = new Function("x", "y", `return Math.sqrt(y)`);
+      return func(x, y);
+    } else if (myFunction.includes("log")) {
+      const func = new Function("x", "y", `return Math.log(y)`);
+      return func(x, y);
+    } else {
+      const func = new Function("x", "y", `return (${myFunction})`);
+      return func(x, y);
+    }
   }
 
   function improvedEulerMethod(h, x0, y0, xTarget) {
@@ -91,6 +112,11 @@ function solveImprovedEuler() {
   document.getElementById("output").innerHTML = results;
 }
 
+
+
+
+
+
 // Runge-Kutta Method
 function solveRK() {
   const myFunction = document.getElementById("functionInput").value;
@@ -101,8 +127,16 @@ function solveRK() {
 
   // Define the derivative function (dy/dx)
   function derivative(x, y) {
-    const func = new Function("x", "y", `return (${myFunction})`);
-    return func(x, y);
+    if (myFunction.includes("sqrt")) {
+      const func = new Function("x", "y", `return Math.sqrt(y)`);
+      return func(x, y);
+    } else if (myFunction.includes("log")) {
+      const func = new Function("x", "y", `return Math.log(y)`);
+      return func(x, y);
+    } else {
+      const func = new Function("x", "y", `return (${myFunction})`);
+      return func(x, y);
+    }
   }
 
   function rkMethod(h, x0, y0, xTarget) {
@@ -112,86 +146,12 @@ function solveRK() {
     let results = "";
 
     while (x <= xTarget) {
-      function solveRK() {
-        const myFunction = document.getElementById("functionInput").value;
-        const x0 = parseFloat(document.getElementById("x0Input").value);
-        const y0 = parseFloat(document.getElementById("y0Input").value);
-        const h = parseFloat(document.getElementById("stepHeightInput").value);
-        const n = parseFloat(document.getElementById("nInput").value);
-      
-        // Define the derivative function (dy/dx)
-        function derivative(x, y) {
-          const func = new Function("x", "y", `return (${myFunction})`);
-          return func(x, y);
-        }
-      
-        function rkMethod(h, x0, y0, xTarget) {
-          let x = x0;
-          let y = y0;
-      
-          let results = "";
-      
-          while (x <= xTarget) {
-            function solveRK() {
-              const myFunction = document.getElementById("functionInput").value;
-              const x0 = parseFloat(document.getElementById("x0Input").value);
-              const y0 = parseFloat(document.getElementById("y0Input").value);
-              const h = parseFloat(document.getElementById("stepHeightInput").value);
-              const n = parseFloat(document.getElementById("nInput").value);
-            
-              // Define the derivative function (dy/dx)
-              function derivative(x, y) {
-                const func = new Function("x", "y", `return (${myFunction})`);
-                return func(x, y);
-              }
-            
-              function rkMethod(h, x0, y0, xTarget) {
-                let x = x0;
-                let y = y0;
-            
-                let results = "";
-            
-                while (x <= xTarget) {
-                  const k1 = h * derivative(x, y);
-                  const k2 = h * derivative(x + h / 2, y + k1 / 2);
-                  const k3 = h * derivative(x + h / 2, y + k2 / 2);
-                  const k4 = h * derivative(x + h, y + k3);
-            
-                  const k = (k1 + 2*k2 + 2*k3 + k4 )/6;
-            
-                  results += `x = ${x.toFixed(4)}, y = ${y.toFixed(4)}<br>`;
-                  y += k;
-                  x += h;
-                  x = parseFloat(x.toFixed(100)); // Handle floating-point precision error
-                }
-            
-                return results;
-              }
-            
-              const xTarget = n;
-            
-              const results = rkMethod(h, x0, y0, xTarget);
-              document.getElementById("output").innerHTML = results;
-            }
-      
-            const k = (k1 + 2*k2 + 2*k3 + k4 )/6;
-      
-            results += `x = ${x.toFixed(4)}, y = ${y.toFixed(4)}<br>`;
-            y += k;
-            x += h;
-            x = parseFloat(x.toFixed(100)); // Handle floating-point precision error
-          }
-      
-          return results;
-        }
-      
-        const xTarget = n;
-      
-        const results = rkMethod(h, x0, y0, xTarget);
-        document.getElementById("output").innerHTML = results;
-      }
+      const k1 = h * derivative(x, y);
+      const k2 = h * derivative(x + h / 2, y + k1 / 2);
+      const k3 = h * derivative(x + h / 2, y + k2 / 2);
+      const k4 = h * derivative(x + h, y + k3);
 
-      const k = (k1 + 2*k2 + 2*k3 + k4 )/6;
+      const k = (k1 + 2 * k2 + 2 * k3 + k4) / 6;
 
       results += `x = ${x.toFixed(4)}, y = ${y.toFixed(4)}<br>`;
       y += k;
